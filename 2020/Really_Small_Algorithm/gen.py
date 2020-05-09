eab0d9d8b3db9a0d36a879d1a0fe7d83d006da6c
BenTechy66 <sendbenspam@yahoo.co.uk>
2020-05-09 18:01:27 +0100

    Add botters challenges

---
+++ 2020/Really_Small_Algorithm/gen.py
@@ -0,0 +1,19 @@
+import rsa
+
+
+FLAG = 'ractf{S0m3t1mesS1zeDoesM4773r}'
+
+
+p = 17
+q = rsa.get_prime()
+e = 65537
+
+state = rsa.solve_for(p=p, q=q, e=e)
+
+pt = int.from_bytes(FLAG.encode(), 'big')
+ct = rsa.encrypt(state, pt, as_bytes=False)
+
+
+print('n:', state[0])
+print('e:', e)
+print('ct:', ct)
