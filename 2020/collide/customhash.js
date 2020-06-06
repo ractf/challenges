97c03359edb6b80f86ca79eb059055e9d54435d7
Daniel Milnes <thebeanogamer@gmail.com>
2020-06-06 01:14:07 +0100

    Delete RACTF

--- 2020/collide/customhash.js
+++
@@ -1,8 +0,0 @@
-const crypto = require("crypto")
-
-exports.hash = function(content) {
-    // Super secret combination of hashing algos
-    let hash = crypto.createHash('sha256').update(content).digest('base64')
-    hash = crypto.createHash('md5').update(hash).digest('hex')
-    return hash.toString().split("").reverse().join("")
-}
